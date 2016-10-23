98fb47afb1406476f4d319c1c7091b3f8cbfb32d
rongbohou <rongbohou@163.com>
2016-10-23 13:47:42 +0800

    pcl_icp

---
+++ build/src/CMakeFiles/interactive_icp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/interactive_icp.dir/interactive_icp.cpp.o"
+  "../../bin/interactive_icp.pdb"
+  "../../bin/interactive_icp"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/interactive_icp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
